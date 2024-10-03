a6323cbcfd8b5d8183c9c21f6f4279bb8b8e1d97
Thanmai1 <Thanmai1@users.noreply.github.com>
2024-10-03 08:18:20 -0500

    Added inv_cost function

--- Work/invcost.py
+++ Work/invcost.py
@@ -1,16 +1,24 @@
 
 import csv
-with open("Data/inventory.csv") as FH:
-    data = csv.reader(FH) # data is a generator object
-    headers = next(data) # store the first line as headers
 
-    total = 0
-    for row in data: # iterating from the second line
-        quant = int(row[1])
-        price = float(row[2])
-        costfor1item = quant*price
-        # print(costfor1item)
-        total = total + costfor1item
+def inventory_cost(filename):
+    with open(filename) as FH:
+        data = csv.reader(FH) # data is a generator object
+        headers = next(data) # store the first line as headers
 
-    print(total)
+        total = 0
+        for row in data: # iterating from the second line
+            quant = int(row[1])
+            price = float(row[2])
+            costfor1item = quant*price
+            # print(costfor1item)
+            total = total + costfor1item
+
+        return total
+
+
+cost = inventory_cost(filename)
+print("Total Cost:", cost)
+
+
 
