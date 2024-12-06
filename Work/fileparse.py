606af443ca031e4b5f7e56a3ff100ff71140e082
Thanmai1 <Thanmai1@users.noreply.github.com>
2024-12-05 21:47:59 -0600

    select subset of cols

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -2,17 +2,28 @@ import sys
 import csv
 
 
-def parse_csv(filename:str) -> list:
+def parse_csv(filename:str,
+              select:list=None,
+              ) -> list:
     ''' Parse a csv file into a list of records'''
     with open(filename) as FH:
         rows = csv.reader(FH)
 
         #Read the file headers
         headers = next(rows)
+        if select:
+            indices = [headers.index(col) for col in select]
+            headers = select
+        else:
+            indices = []
+
         records = []
         for row in rows:
             if not row:
                 continue
+
+            if indices:
+                row = [row[idx] for idx in indices]
 
             record = dict(zip(headers, row))
             records.append(record)
