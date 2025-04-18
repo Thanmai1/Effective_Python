975c8d0b009955f2a4ecbd1b96a48be7cd790b7f
Thanmai1 <Thanmai1@users.noreply.github.com>
2025-04-17 21:51:49 -0500

    making inventory a container

--- Work/inventory.py
+++ Work/inventory.py
@@ -5,6 +5,19 @@ class Inventory:
     def __iter__(self):
         return self._products.__iter__()
 
+    def __len__(self):
+        return len(self._products)
+
+    def __getitem__(self, index):
+        return self._products[index]
+
+    def __contains__(self, val):
+        return any(val == p.name for p in self._products )
+
+
+
+
+
     @property
     def total_cost(self):
         return sum( p.cost for p in self._products)
--- Work/product.py
+++ Work/product.py
@@ -6,6 +6,9 @@ class Product:
         self.quant = quant
         self.price = price
 
+    def __repr__(self):
+        return f'Product({self.name}, {self.quant}, {self.price})'
+
 
     @property
     def quant(self):
